00d760bb5a431b1712467b3ab4f80686e31b8acc
Kleyvisson Matias <kleyvissonmatias@gmail.com>
2023-09-18 03:00:33 -0300

    Ajustando controllers

--- application/src/main/java/br/com/ministerio/recomeco/presentation/CelulaController.java
+++ application/src/main/java/br/com/ministerio/recomeco/presentation/CelulaController.java
@@ -1,7 +1,16 @@
 package br.com.ministerio.recomeco.presentation;
 
+import br.com.ministerio.recomeco.service.CelulaService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Slf4j
 public class CelulaController {
+
+    private CelulaService service;
 }
--- application/src/main/java/br/com/ministerio/recomeco/presentation/VidaController.java
+++ application/src/main/java/br/com/ministerio/recomeco/presentation/VidaController.java
@@ -1,10 +1,20 @@
 package br.com.ministerio.recomeco.presentation;
 
+import br.com.ministerio.recomeco.service.VidaService;
 import br.com.ministerio.recomeco.util.Utils;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Slf4j
 public class VidaController {
+
+    private VidaService service;
+
     public void Teste () {
         Utils.retornaString();
     }
