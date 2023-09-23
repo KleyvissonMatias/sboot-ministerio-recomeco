29bbda982b0fb8605ea94e81c907299925eff6f8
Kleyvisson Matias <kleyvissonmatias@gmail.com>
2023-09-23 00:26:29 -0300

    - Ajustando retorno de mensagem de erro;

--- domain/src/main/java/br/com/ministerio/recomeco/domain/response/ErroResponse.java
+++ domain/src/main/java/br/com/ministerio/recomeco/domain/response/ErroResponse.java
@@ -3,12 +3,14 @@ package br.com.ministerio.recomeco.domain.response;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.Serializable;
 
 @Builder
 @Getter
 @Setter
+@Slf4j
 public class ErroResponse implements Serializable {
     private Integer codigo;
     private String retorno;
